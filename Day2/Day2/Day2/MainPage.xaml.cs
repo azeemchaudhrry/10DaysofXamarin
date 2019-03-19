1d3d1bef4cd9369a49d35c3a56b9242119a43590
azeem <azeem@spica>
2019-03-19 11:36:16 +0400

    day 2 coding challange

---
+++ Day2/Day2/Day2/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Day2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfTextAvailable();
+        }
+
+        private void ContentEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfTextAvailable();
+        }
+
+        private void CheckIfTextAvailable()
+        {
+            saveBtn.IsEnabled = (!string.IsNullOrEmpty(titleEntry.Text) && !string.IsNullOrEmpty(contentEditor.Text));
+        }
+
+        private void SaveBtn_OnClicked(object sender, EventArgs e)
+        {
+            titleEntry.Text = string.Empty;
+            contentEditor.Text = string.Empty;
+        }
+    }
+}
